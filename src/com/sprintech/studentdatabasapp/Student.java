47c975db3a2060acdbefbe435a40349e1d02a16c
AKIBIRIO <ayubkibirio@gmail.com>
2023-08-11 17:32:48 +0300

    added logic to enroll into a course

--- src/com/sprintech/studentdatabasapp/Student.java
+++ src/com/sprintech/studentdatabasapp/Student.java
@@ -7,14 +7,14 @@ public class Student {
     private String firstName;
     private String lastName;
     private int gradeYear;
-    private String courses;
-    private String studentID;   
-    private int tuitionBalance;
-    private static int costOfCourse=600;
-    private static int id = 1000;  //value belongs to the class, its true for all objects
+    private String courses = null;
+    private String studentID;
+    private int tuitionBalance = 0;
+    private static int costOfCourse = 600;
+    private static int id = 1000; // value belongs to the class, its true for all objects
 
-    //Constructor prompts user to enter student's name and year
-    public Student(){
+    // Constructor prompts user to enter student's name and year
+    public Student() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter Student FirstName: ");
         this.firstName = input.nextLine();
@@ -23,25 +23,42 @@ public class Student {
         this.lastName = input.nextLine();
 
         System.out.println("1- Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter Student Class level: ");
-        this.gradeYear=input.nextInt();
-         setStudentID();
-        System.out.println( firstName + " " + lastName + " " + gradeYear + " "+ studentID);
-       
-        input.close();
-    }
-
-    //Generate an ID
-    private void setStudentID(){
-        //Grade Level + ID
-         id++;
-        this.studentID =  gradeYear + "" + id;
+        this.gradeYear = input.nextInt();
+        setStudentID();
+        System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
 
     }
 
-    //Enroll in courses
+    // Generate an ID
+    private void setStudentID() {
+        // Grade Level + ID
+        id++;
+        this.studentID = gradeYear + "" + id;
 
-    //View Balance
-    //Pay tution
+    }
 
-    //show status
+    // Enroll in courses
+    public void enrollToCourse(){
+        //Get inside the loop, user hits 0
+        do{
+            System.out.print("Enter course to enroll(Q to quit): ");
+            Scanner input = new Scanner(System.in);
+            String course = input.nextLine();
+            if (!course.equals( "Q")){
+                courses = courses + "\n" + course;
+                tuitionBalance = tuitionBalance + costOfCourse;           
+            }else{
+                break;
+            }
+        }while(1 !=0);
+         System.out.println("Enrolled in: " + courses);
+         System.out.println("Tuition Balance: " + tuitionBalance);
+
+         
+    }
+
+    // View Balance
+    // Pay tution
+
+    // show status
 }
--- src/com/sprintech/studentdatabasapp/StudentDatabaseApp.java
+++ src/com/sprintech/studentdatabasapp/StudentDatabaseApp.java
@@ -4,6 +4,7 @@ public class StudentDatabaseApp {
 
     public static void main(String[] args) {
         Student student = new Student();
+        student.enrollToCourse();
         //Ask How many users we want to add
 
         //create n number of students
